d18ff1676f1049871dc9b573ae82711f62ebc9c2
Eduardo Cabrera <lalo_cabrera1977@hotmail.com>
2025-02-08 23:06:01 -0600

    Solution to the problem: 8. Weather Observation Station 3 (Easy)

---
+++ Sql/8. Weather Observation Station 3 (Easy).sql
@@ -0,0 +1,7 @@
+/*
+    DESCRIPTION: Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT DISTINCT CITY FROM STATION GROUP BY ID, CITY HAVING ((ID % 2) = 0);
